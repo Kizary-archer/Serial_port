b9de4041ad6a7437e35fca4253b2679f45ce81df
Unknown <Kizary-archer@yandex.ru>
2019-10-28 23:27:23 +0300

    listener fix

--- src/main/java/SerialPort/ComPortListenerServise.java
+++ src/main/java/SerialPort/ComPortListenerServise.java
@@ -1,4 +1,32 @@
 package SerialPort;
 
-public class ComPortListenerServise {
+import com.fazecast.jSerialComm.SerialPort;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.ScheduledService;
+import javafx.concurrent.Task;
+
+public class ComPortListenerServise extends ScheduledService{
+    private SerialPort comPort = null;
+
+    ComPortListenerServise(String comName){
+        this.comPort = SerialPort.getCommPort(comName);
+    }
+
+    ComPortListenerServise() { this.comPort = null;}
+
+    void setComPortName(String comName){this.comPort = SerialPort.getCommPort(comName);}
+    @Override
+    protected Task createTask() {
+        return  new  Task<String> () {
+            @Override
+            protected String call() throws Exception {
+                if(comPort ==null){System.out.println("dddddddddddddddddd");}
+                System.out.println(comPort.getSystemPortName());
+                return "bkj";
+            }
+
+
+        };
+    }
 }
