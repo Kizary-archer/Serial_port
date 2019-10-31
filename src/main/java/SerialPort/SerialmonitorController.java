0207bf358e1524984530f4d11aa4cece59ee0f16
Unknown <Kizary-archer@yandex.ru>
2019-10-31 20:33:49 +0300

    gitignore

--- src/main/java/SerialPort/SerialmonitorController.java
+++ src/main/java/SerialPort/SerialmonitorController.java
@@ -1,16 +1,14 @@
 package SerialPort;
 
 
-import com.fazecast.jSerialComm.SerialPort;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.control.*;
-import javafx.scene.effect.BlendMode;
-import javafx.scene.effect.BlendMode;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
